dfa7ee839b64f7700530de5820b1faf5f9219dd5
Ingrid Amaral <guid_95@hotmail.com>
2020-02-14 14:57:33 +0100

    approval card

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import faker from 'faker'
+import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
     return (
         <div className='ui container comments'>
-            <div className='comment'>
-                <a href='/' className='avatar'>
-                    <img alt='avatar' src={faker.image.avatar()} />
-                </a>
-                <div className='content'>
-                    <a href='/' className='author'>
-                        Sam
-                    </a>
-                    <div className='metadata'>
-                        <span className='date'>Today at 06:00PM</span>
-                    </div>
-                    <div className='text'>Nice Blog Post!</div>
-                </div>
-            </div>
+            <ApprovalCard>
+                <CommentDetail 
+                name={faker.name.firstName()+' '+faker.name.lastName()} 
+                avatar={faker.image.avatar()}
+                comment={faker.lorem.sentence()} />
+            </ApprovalCard>
+            
+            <CommentDetail 
+              name={faker.name.firstName()+' '+faker.name.lastName()} 
+              avatar={faker.image.avatar()}
+              comment={faker.lorem.sentence()} />
+            <CommentDetail 
+              name={faker.name.firstName()+' '+faker.name.lastName()} 
+              avatar={faker.image.avatar()}
+              comment={faker.lorem.sentence()} />
         </div>
     ) 
 }
